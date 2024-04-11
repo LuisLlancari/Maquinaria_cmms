fe20ee0019ab5c0df8e52ba0afa15a70541e7531
LuisLlancari <miguel.llancari12@gmail.com>
2024-04-11 18:53:22 -0500

    avanzes con la vista

--- CMMSBeta/implemento/views.py
+++ CMMSBeta/implemento/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from . models import TipoImplemento, Implemento, DetImplementos
 
 # verifica si el usuario esta logueado
 @login_required(login_url='login', redirect_field_name='implemento')
 # definimos la vista
 def implento(request):
+  
+
   # retornamos el render de la plantilla de nuestra vista
   return render(request, 'implemento/implemento.html')
 
 def tipoImplemento (request):
+  datos_tipoimplemento = TipoImplemento.objects.filter(estado = True   )
   return render(request, 'implemento/tipoimplemento.html')
 
 def detalleImplemento (request):
