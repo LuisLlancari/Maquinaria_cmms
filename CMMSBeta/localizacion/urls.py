6acd84c89fedc025d58b4bcf8f9751a4e4af9b91
fabianjesu <fabianjesuspurilla@gmail.com>
2024-04-12 12:45:44 -0500

    fabian

--- CMMSBeta/localizacion/urls.py
+++ CMMSBeta/localizacion/urls.py
@@ -4,5 +4,7 @@ from .views import views, sede
 urlpatterns = [
     path('', views.area, name='area'),
     path('base', views.base, name='base'),
-     path('sede', sede.sede , name='sede'),
+    path('sede', sede.sede , name='sede'),
+    path('eliminar_sede/<int:idsede>', sede.eliminar_sede, name='eliminar_sede'), 
+    path('registrar_sede', sede.registrar_sede, name='registrar_sede'), 
 ]
--- CMMSBeta/localizacion/views/sede.py
+++ CMMSBeta/localizacion/views/sede.py
@@ -4,6 +4,29 @@ from ..forms import SedeForm
 
 def sede(request):
     sedes = Sede.objects.all()
-    return render(request, '../templates/localizacion/sede.html', {'sedes': sedes})
+    form = SedeForm()
+    return render(request, '../templates/localizacion/sede.html', {'sedes': sedes, 'form': form})
+
+def eliminar_sede(request, idsede):
+    sede = get_object_or_404(Sede, pk = idsede)
+    if request.method == 'POST':
+        sede.delete()
+        return redirect('sede')
+    
+    return render(request, '', {'sede': sede})
+
+def registrar_sede(request):
+    if request.method == 'POST':
+        form = SedeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('sede')
+    else:
+        form = SedeForm()
+        return render(request, 'localizacion/registrar_sede.html', {'form': form})
+    
+    
+    
+    
 
         
