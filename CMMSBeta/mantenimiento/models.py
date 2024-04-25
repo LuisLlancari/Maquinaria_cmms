4b20d78293ab1deed485f450ce0c3ddcacb46f09
LuisLlancari <miguel.llancari12@gmail.com>
2024-04-24 23:08:53 -0500

    modelo de mantenimiento

--- CMMSBeta/mantenimiento/models.py
+++ CMMSBeta/mantenimiento/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from usuario.models import Usuario, Persona
+
 
 # Create your models here.
+class Mantenimiento(models.Model):
+  idmantenimiento = models.AutoField(primary_key=True)
+  idmanpreventivo = models.ForeignKey( , on_delete=models.SET_DEFAULT, default=None, verbose_name="mantenimiento preventivo")
+  fechasalida = models.DateField(auto_now_add=True, null=True, verbose_name="Fecha de creación")
+  idpersona = models.ForeignKey(Persona, on_delete=models.SET_DEFAULT, default=None, verbose_name="Persona")
+  descripcion = models.CharField(max_length=60, verbose_name="Descripcion")
+  estado = models.BooleanField(default=True, verbose_name="Estado")
+
+  class Meta:
+          verbose_name = "Mantenimiento"
+          verbose_name_plural = "Mantenimientos"
+
+  def __str__(self):
+      return self.idmantenimiento
