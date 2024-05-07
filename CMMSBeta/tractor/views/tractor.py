f62e74d6389dd8cd2604bc5f09922453ab7b421b
HaroldQn <efrainqn16@gmail.com>
2024-05-07 10:19:22 -0500

    cambios

--- CMMSBeta/tractor/views/tractor.py
+++ CMMSBeta/tractor/views/tractor.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from fundo_cultivo.models import Fundo
 from usuario.models import Usuario
 
+from django.http import HttpResponse
 @login_required(login_url='login', redirect_field_name='')
 def tractor(request):
 
@@ -73,17 +74,28 @@ def registrar_tractor(request):
 def editar_tractor(request):
     if request.method == 'POST':
         idtractor = request.POST.get('idtractor')
+
+        nom_tractor = request.POST.get('nrotractor').strip()
+        idusuario = request.POST.get('idusuario')
+        idfundo = request.POST.get('idfundo')
+
+        print(nom_tractor)
+        print(idusuario)
+        print(idfundo)
         tractor = get_object_or_404(Tractor, pk = idtractor)
         form = TractorForm(request.POST, instance=tractor)
 
-        nom_tractor = request.POST.get('nrotractor').strip()
-        existe_tractor = Tractor.objects.filter(nrotractor = nom_tractor, estado = True).exists()
+        existe_tractor = Tractor.objects.filter(nrotractor = nom_tractor, idusuario = idusuario, idfundo = idfundo, estado = True).exists()
+        print(existe_tractor)
         if form.is_valid() and existe_tractor == False:
             form.save()
             messages.success(request, "El tractor ha sido modificado correctamente", extra_tags='primary')
             return redirect('tractor')
         else:
-            messages.success(request, "El tractor ya existe", extra_tags='warning')
+            # errores = form.errors.as_text()
+            # mensaje_error = f"Hubo un error: {errores}"
+            # return HttpResponse(mensaje_error)
+            messages.success(request, "El tractor ya existe ", extra_tags='warning')
             return redirect('tractor')
     else:
         return redirect('tractor')
