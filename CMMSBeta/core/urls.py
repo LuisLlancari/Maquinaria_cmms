fbde758557043177da7503aecff65b759cc33684
LuisLlancari <miguel.llancari12@gmail.com>
2024-05-14 11:28:48 -0500

    añadiendo graficos

--- CMMSBeta/core/urls.py
+++ CMMSBeta/core/urls.py
@@ -5,6 +5,10 @@ from .view import reportes
 urlpatterns = [
    path('', views.home, name="home"),
    path('home/<datagrafic>/', views.home, name='home_with_datagrafic'),
+
+   path('datos_graficos/<str:fecha>/<int:supervisor>/<str:turnos>', views.datos_grafico, name='datos_grafico'),
+   path('datos_tabla/<str:fecha>/<int:supervisor>/<str:turno>', views.datos_tabla, name='datos_tabla'),
+
    path('test', views.test, name="test"),
    path('Exportar', reportes.exportar, name="exportar"),
    path('exportpdf', reportes.reportePDF, name="exportpdf"),
