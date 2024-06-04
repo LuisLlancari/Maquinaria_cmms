d8dbf975639c1e70bbf3e2218191a4f6adbcad94
HaroldQn <efrainqn16@gmail.com>
2024-06-03 22:42:56 -0500

    cambios en el sidebar - mejora

--- CMMSBeta/CMMSBeta/settings.py
+++ CMMSBeta/CMMSBeta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dj9+y84%liifi)v=evex(z_!)s$085jyu8&p68q191)qaxmw-b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '172.20.10.7', '192.168.137.81']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '172.20.10.7', '192.168.137.81', '172.17.192.1','192.168.18.83']
 
 
 
